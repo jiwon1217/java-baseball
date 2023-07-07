f6e59a5b80239bcf735f217078a1154ebef023aa
jiwon1217 <koagjw@gmail.com>
2023-07-07 21:58:27 +0900

    refactor(baseball): addUniqueNumber() -> HashSet 사용, do-while -> while 변경

--- src/main/java/baseball/util/NumberGenerator.java
+++ src/main/java/baseball/util/NumberGenerator.java
@@ -2,26 +2,21 @@ package baseball.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NumberGenerator {
-
     private static final int RANDOM_LOWER_INCLUSIVE = 1;
     private static final int RANDOM_UPPER_INCLUSIVE = 9;
     private static final int MAX_SIZE = 3;
 
     public static List<Integer> generateRandomNumber() {
-        List<Integer> numbers = new ArrayList<>();
-        do {
+        Set<Integer> numberSet = new HashSet<>();
+        while (numberSet.size() < MAX_SIZE) {
             int number = Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
-            addUniqueNumber(numbers, number);
-        } while (numbers.size() < MAX_SIZE);
-        return numbers;
-    }
-
-    private static void addUniqueNumber(final List<Integer> numbers, final int number) {
-        if (!numbers.contains(number)) {
-            numbers.add(number);
+            numberSet.add(number);
         }
+        return new ArrayList<>(numberSet);
     }
 }
